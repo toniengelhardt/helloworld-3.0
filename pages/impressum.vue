8b5f1295b99da638919bc05d75707fa27bf1d5c9
Toni Engelhardt <toni88x@gmail.com>
2022-05-18 20:47:51 +0100

    Impressum

---
+++ pages/impressum.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="min-h-screen place-content-center flex flex-col items-center justify-center">
+    <div class="text-center">
+      <p class="mb-8">
+        <NuxtLink
+          to="/"
+          class="text-4xl"
+        >
+          <i class="i-carbon-arrow-left" />
+        </NuxtLink>
+      </p>
+      <h1 class="text-3xl">
+        Impressum
+      </h1>
+      <p class="mt-8">
+        Toni Engelhardt
+      </p>
+      <p class="mt-2">
+        Rua Poço dos Negros 153 / 2°
+      </p>
+      <p class="mt-2">
+        1200-337 Lisbon, Portugal
+      </p>
+    </div>
+  </div>
+</template>
