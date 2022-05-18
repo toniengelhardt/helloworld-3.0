a0850854be880eb231cf7055022056560254f113
Toni Engelhardt <toni88x@gmail.com>
2022-05-18 21:02:33 +0100

    Add attribution

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -21,6 +21,15 @@
       <p class="mt-2">
         1200-337 Lisbon, Portugal
       </p>
+      <p class="mt-8 text-sm">
+        Made with
+        <span class="nowrap"><i class="i-logos-nuxt-icon" /> <a href="https://v3.nuxtjs.org/" class="font-bold underline" title="Nuxt 3 – The hybrid Vue framework" rel="noopener noreferrer">Nuxt</a></span>
+        <!-- <span class="nowrap"><i class="i-logos-vitejs" /> <a href="https://v3.nuxtjs.org/" class="font-bold underline" title="Vite – Next generation frontend tooling" rel="noopener noreferrer">Vite</a></span> -->
+      </p>
+      <p class="mt-2 text-sm">
+        Deployed on
+        <span class="nowrap"><i class="i-logos-vercel-icon" /> <a href="https://vercel.com" class="font-bold underline" title="Vercel – Deployment platform" rel="noopener noreferrer">Vercel</a></span>
+      </p>
     </div>
   </div>
 </template>
